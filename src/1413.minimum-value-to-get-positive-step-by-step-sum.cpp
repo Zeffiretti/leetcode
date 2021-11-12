2043ce4da98ed732a61bbba5abb5a4d5a0eae1cf
zeffiretti <hiesh@mail.com>
2021-11-12 19:37:32 +0800

    solve pro. 1413

---
+++ src/1413.minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=1413 lang=cpp
+ *
+ * [1413] Minimum Value to Get Positive Step by Step Sum
+ */
+#include "utils.h"
+// @lc code=start
+class Solution {
+ public:
+  int minStartValue(vector<int> &nums) {
+    int m_min = 0;
+    int integ = 0;
+    for (auto &i : nums) {
+      integ += i;
+      m_min = min(m_min, integ);
+    }
+    return max(1, 1 - m_min);
+  }
+};
+// @lc code=end
+
