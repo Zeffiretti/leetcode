34593f74f597fc3adf63f10f206661c71aab8ec5
zeffiretti <hiesh@mail.com>
2021-11-13 23:19:08 +0800

    Solve Pro. 203

--- include/utils.h
+++ include/utils.h
@@ -7,4 +7,12 @@
 #include <cmath>
 #include <algorithm>
 using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 #endif
---
+++ src/203.remove-linked-list-elements.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=203 lang=cpp
+ *
+ * [203] Remove Linked List Elements
+ */
+#include "utils.h"
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode *removeElements(ListNode *head, int val) {
+    ListNode *dummy = new ListNode(-1, head), *prev = dummy;
+    for (; head; head = head -> next)
+      if (head -> val != val)
+        prev = head;
+      else
+        prev -> next = head -> next;
+    return dummy -> next;
+  }
+};
+// @lc code=end
+
