08da7b945afd750d1fa427d5855549df64179820
zeffiretti <hiesh@mail.com>
2021-11-20 20:32:37 +0800

    Solve Pro. 461

---
+++ src/461.hamming-distance.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=461 lang=cpp
+ *
+ * [461] Hamming Distance
+ */
+#include "utils.h"
+// @lc code=start
+class Solution {
+ public:
+  int hammingDistance(int x, int y) {
+    int Xor = x ^ y, ans = 0;
+    while (Xor)
+      ans++, Xor &= Xor - 1;
+    return ans;
+  }
+};
+// @lc code=end
+
