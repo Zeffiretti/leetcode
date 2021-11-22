3c105589f8e9ac2c74f6eb50c93036348e379168
zeffiretti <hiesh@mail.com>
2021-11-22 21:54:58 +0800

    Solve Pro. 106

--- include/utils.h
+++ include/utils.h
@@ -16,4 +16,15 @@ struct ListNode {
   ListNode(int x) : val(x), next(nullptr) {}
   ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 #endif
