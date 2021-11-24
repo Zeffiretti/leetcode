cfcd2bfe119f1cf7978b73ee6594f65f4ba970b8
zeffiretti <hiesh@mail.com>
2021-11-24 21:01:35 +0800

    Solve Pro. 8

---
+++ src/8.string-to-integer-atoi.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=8 lang=cpp
+ *
+ * [8] String to Integer (atoi)
+ */
+#include "utils.h"
+// @lc code=start
+class Solution {
+ public:
+  int myAtoi(string s) {
+    int i = 0;
+    long long num = 0;
+    while (s[i] == ' ') {
+      i++;
+    }
+    bool isNegative = s[i] == '-';
+    if (isNegative || s[i] == '+') {
+      i++;
+    }
+    while ((s[i] - '0') >= 0 && (s[i] - '0') <= 9) {
+      if (((num * 10) + (s[i] - '0')) > INT_MAX)
+        return isNegative ? INT_MIN : INT_MAX;
+      num = num * 10 + (s[i] - '0');
+      i++;
+    }
+    return isNegative ? -num : num;
+  }
+};
+// @lc code=end
+
