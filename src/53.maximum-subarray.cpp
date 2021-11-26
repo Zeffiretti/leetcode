6430c3ede0164da9db4f14d856c46620771ebec7
Zeffiretti <hiesh@mail.com>
2021-11-26 20:40:19 +0800

    Solve Pro. 53

---
+++ src/53.maximum-subarray.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=53 lang=cpp
+ *
+ * [53] Maximum Subarray
+ */
+#include "utils.h"
+// @lc code=start
+class Solution {
+ public:
+  int maxSubArray(vector<int> &nums) {
+    if (nums.size() == 0) return NULL;
+    int res = INT_MIN;
+    int f_n = -1;
+    for (int i = 0; i < nums.size(); ++i) {
+      f_n = max(nums[i], f_n + nums[i]);
+      res = max(f_n, res);
+    }
+    return res;
+  }
+};
+// @lc code=end
+
+
+int main(int argc, char **argv) {
+  vector<int> nums = {0, 0, 0, 0, 0, -2, 1, -3, 4, -1, 0, 0, 0, 0, 2, 1, -5, 4, 0, 0, 0, 0};
+  Solution ss;
+  cout << ss.maxSubArray(nums);
+  return 0;
+}
