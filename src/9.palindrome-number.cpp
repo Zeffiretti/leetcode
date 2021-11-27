d58c42e84ebb1f8c8d9eaa64bdae2ab21e84e68e
Zeffiretti <hiesh@mail.com>
2021-11-27 18:16:11 +0800

    Solve Pro. 9

---
+++ src/9.palindrome-number.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=9 lang=cpp
+ *
+ * [9] Palindrome Number
+ */
+#include "utils.h"
+// @lc code=start
+class Solution {
+ public:
+  int reverse(int x) {
+    int ret = 0, max = 0x7fffffff, min = 0;
+    long rs = 0;
+    for (; x; rs = rs * 10 + x % 10, x /= 10);
+    return ret = rs > max || rs < min ? 0 : rs;
+  }
+  bool isPalindrome(int x) {
+    return x == reverse(x);
+  }
+};
+// @lc code=end
+
