43e47bd79b43e8fd665cc47ae7e8944fd5a11d97
Zeffiretti <hiesh@mail.com>
2021-11-27 18:51:54 +0800

    Solve Pro. 238

---
+++ src/238.product-of-array-except-self.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=238 lang=cpp
+ *
+ * [238] Product of Array Except Self
+ */
+#include "utils.h"
+// @lc code=start
+class Solution {
+ public:
+  vector<int> productExceptSelf(vector<int> &nums) {
+    // int len = nums.size();//[1234]
+    vector<int> res(nums.size(), 1);
+    int left = 1;
+    int right = 1;
+    for (int i = 0; i < nums.size(); ++i) {
+      res[i] *= left;
+      left *= nums[i];
+      res[nums.size() - i - 1] *= right;
+      right *= nums[nums.size() - i - 1];
+    }
+    return res;
+  }
+};
+// @lc code=end
+
