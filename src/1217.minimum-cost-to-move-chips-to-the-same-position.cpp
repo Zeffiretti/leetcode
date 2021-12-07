95bf0ec59683bff04ac2d5c34a6e4cf0e4a29f0f
Zeffiretti <hiesh@mail.com>
2021-12-07 23:33:49 +0800

    Solve Pro. 1217

---
+++ src/1217.minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1217 lang=cpp
+ *
+ * [1217] Minimum Cost to Move Chips to The Same Position
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int minCostToMoveChips(vector<int> &position) {
+    int c[2] = {0};
+    for (auto pos : position) {
+      ++c[pos % 2];
+    }
+    return min(c[0], c[1]);
+  }
+};
+// @lc code=end
+
