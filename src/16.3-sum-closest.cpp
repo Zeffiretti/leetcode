6a4b9278e92e7c2ef133e5ad07a5be5ef62f7fb8
Zeffiretti <hiesh@mail.com>
2022-03-16 10:49:36 +0800

    add all files as library in CMakeLists.txt

---
+++ src/16.3-sum-closest.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=16 lang=cpp
+ *
+ * [16] 3Sum Closest
+ */
+#include "utils.h"
+// @lc code=start
+class Solution {
+ public:
+  int threeSumClosest(vector<int> &nums, int target) {
+    int res = INT_MAX;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size() - 2; i++) {
+      if (i > 0 && nums[i] == nums[i - 1]) {
+        continue;
+      }
+      int left = i + 1;
+      int right = nums.size() - 1;
+      while (left < right) {
+        int sum = nums[i] + nums[left] + nums[right];
+        if (sum == target) {
+          return sum;
+        } else if (sum < target) {
+          left++;
+        } else {
+          right--;
+        }
+        if (abs(sum - target) < abs(res - target)) {
+          res = sum;
+        }
+      }
+    }
+    return res;
+  }
+};
+// @lc code=end
+
